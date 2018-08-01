720b26893d57c9168e744a311d193abf6f4bac2b
Paul <hoffsbox@gmail.com>
2018-08-01 16:40:58 -0400

    Directory Structure

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,17 @@
 /* Run this file to create the burgers_db database for the Eat-Da_Burger app  */
 
 INSERT INTO burgers (burger_name, devoured)
-VALUES("Swenson's Gally Boy", false);
+VALUES("Big Kahuna Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Royal With Cheese", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Swenson's Galley Boy", false);
 
 INSERT INTO burgers (burger_name, devoured)
 VALUES("Skyway Sky High", false);
 
 INSERT INTO burgers (burger_name, devoured)
 VALUES("Whitey's Shack Burger", false);
-
-INSERT INTO burgers (burger_name, devoured)
-VALUES("Louie's Mushroom Burger", false);
-
-INSERT INTO burgers (burger_name, devoured)
-VALUES("Bob's Knife & FOrk Burger", false);
 
