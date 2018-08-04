b865255ea59e0b67cad85294d0f21eedc6434c76
Paul <hoffsbox@gmail.com>
2018-08-04 14:31:20 -0400

    Error Corrected

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -19,6 +19,30 @@
         });
     });
 
+    router.post("api/burgers/post", function(req, res) {
+        burger.create([req.body.name], function(result){
+            res.json({ id: result.insertId });
+        });
+    });
+
+    router.put("api/burgers/;id", function(result) {
+        var condition = "id = " + req.params.id;
+
+        console.log("condition", condition);
+    })
+
+    router.delete("api/burger/:id", function(req, res) {
+        var conditioni = "id = " + req.params.id;
+
+        burger.delete(condition, function(result) {
+            if (result.affectedRows == 0) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        });
+    });
+
 // Export routes for server.js to use
 
     module.exports = router;
